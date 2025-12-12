89aee316b781f27e1ced500e274d4431a7d706be
cosmearcade8-hue <cosmearcade8@gmail.com>
2025-12-12 14:06:58 +0800

    update ko

--- my/src/main.tsx
+++ my/src/main.tsx
@@ -1,17 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { createTheme, MantineProvider } from '@mantine/core';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createTheme, MantineProvider } from "@mantine/core";
 
-const theme = createTheme({
-  /** Put your mantine theme override here */
-});
-createRoot(document.getElementById('root')!).render(
-   <MantineProvider theme={theme}>
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const theme = createTheme({});
+
+createRoot(document.getElementById("root")!).render(
+  <MantineProvider theme={theme}>
+    <StrictMode>
+      <App />
+    </StrictMode>
   </MantineProvider>
-)
- 
+);
